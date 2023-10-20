caf1e7f63a17aee661f8ec925982b4894d096e55
Renan Ladislau <renanladislau9@gmail.com>
2023-10-20 00:58:48 -0300

    Alterei o css

--- src/Componentes/TerceiraSecao.js
+++ src/Componentes/TerceiraSecao.js
@@ -1,9 +1,16 @@
 import Style from '../EstilosComponentes/TerceiraSecao.module.css'
 import Celular2 from '../Img/celular2.png'
+import {React, useEffect} from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 function TerceiraSecao(){
+    useEffect(() => {
+        AOS.init();
+      }, [])
     return(
         <div className={Style.TerceiraSecao}>
-            <p className={Style.TextoTerceiraSecao}>Desbloqueie o <span className={Style.Diferente1}> poder </span> da <span className={Style.Diferente1}> tecnologia </span> no seu pulso e no seu <span className={Style.Diferente1}> bolso </span> com nosso banco digital!</p>
+            <p className={Style.TextoTerceiraSecao} data-aos="zoom-in" data-aos-duration="1000">Desbloqueie o <span className={Style.Diferente1}> poder </span> da <span className={Style.Diferente1}> tecnologia </span> no seu pulso e no seu <span className={Style.Diferente1}> bolso </span> com nosso banco digital!</p>
             <div className={Style.Celular2}>
                 <img src={Celular2} alt="" />
             </div>
